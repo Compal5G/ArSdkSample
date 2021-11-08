0303bb1a8f221f6dbb316bb996bd224063faf129
stephen_hsu <stephen_hsu@compal.com>
2021-11-08 10:15:33 +0800

    1.UnityChan_SVR  2.Forklift_SVR  3.Navgation_Calibration3D  4.SpotterMachineModel_Calibration3D

---
+++ Assets/Scenes/Nav3D/NavCode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;    
+
+
+
+public class NavCode : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float startWidth=1, endWidth=1;
+    [SerializeField]
+    Material material;
+    [SerializeField]
+    GameObject ARGameObjectLock;
+
+    private NavMeshPath path;
+    private LineRenderer line;
+    void Start()
+    {
+        line = gameObject.AddComponent<LineRenderer>();
+        path = new NavMeshPath();
+        //只有設置了材質 setColor纔有作用
+        line.startWidth = startWidth;//設置直線寬度
+        line.endWidth = endWidth;//設置直線寬度
+        line.material = material;
+    }
+
+    void Update()
+    {
+        if (ARGameObjectLock.transform.localScale.x != 0)
+        {
+            NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            for (int i = 0; i < path.corners.Length; i++)
+            {
+                line.SetPosition(i, path.corners[i]);
+            }
+        }
+           }
+    }
---
+++ Assets/Scripts/DemoScripts_Forklift/CubeEventForGazeAndGestureForklift.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeEventForGazeAndGestureForklift : MonoBehaviour
+{
+    [SerializeField] CameraGazeHandler gazeHandler;
+    [SerializeField] GameObject cube0, cube1;
+    [SerializeField] Transform fork;
+    [SerializeField] bool oneShot = false;
+    [SerializeField] bool test = false;
+    [SerializeField] GameObject oneShotGameObject = null;
+    private GestureInfo currGes;
+
+    void Start()
+    {
+        gazeHandler.StartGazeEvent += StartGazeEvent;
+        gazeHandler.EndGazeEvent += EndGazeEvent;
+    }
+
+
+    void Update()
+    {
+        GestureInfo newGes = XvGesture.GetGesture();
+        if (newGes.id != currGes.id)
+        {
+            fork.localPosition = new Vector3(0.9635f, 2.2295f, 0);
+        }
+        currGes = newGes;
+
+        if (startUpMoving)
+        {
+            if (fork.localPosition.y <= 6)
+            {
+                fork.position += new Vector3(0, 0.01f, 0);
+            }
+        }
+        if (startDownMoving)
+        {
+            if (fork.localPosition.y >= 2.2295f)
+            {
+                fork.position -= new Vector3(0, 0.01f, 0);
+            }
+        }
+    }
+
+    bool startUpMoving = false;
+    bool startDownMoving = false;
+    void StartGazeEvent(Collider obj)
+    {
+        if (oneShot == false && obj.gameObject != oneShotGameObject)
+        {
+            oneShot = true;
+            oneShotGameObject = obj.gameObject;
+            if (obj.gameObject == cube0)
+            {
+                startUpMoving = true;
+            }
+            else if (obj.gameObject == cube1)
+            {
+                startDownMoving = true;
+            }
+            else
+            {
+                startUpMoving = false;
+                startDownMoving = false;
+                oneShot = false;
+                oneShotGameObject = null;
+            }
+        }
+    }
+
+    void EndGazeEvent(Collider obj)
+    {
+        startUpMoving = false;
+        startDownMoving = false;   
+        oneShot = false;
+        oneShotGameObject = null;
+    }
+
+    public void testYA()
+    {
+        
+    }
+}
