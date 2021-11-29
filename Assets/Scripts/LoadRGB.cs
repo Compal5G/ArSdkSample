81a1f9006929e0ffd60ca824af78065794d0b845
jorseph_hsu <jorseph_hsu@compal.com>
2021-11-29 17:36:50 +0800

    for RGB error.

--- Assets/Scripts/LoadRGB.cs
+++ Assets/Scripts/LoadRGB.cs
@@ -5,6 +5,7 @@ using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Object = UnityEngine.Object;
+using System.IO;
 
 public class LoadRGB : MonoBehaviour, SvrManager.SvrEventListener
 {
@@ -16,12 +17,15 @@ public class LoadRGB : MonoBehaviour, SvrManager.SvrEventListener
     private int lastWidth = 0;
     private int lastHeight = 0;
     private Thread newThread;
-    private Thread newThread2;
     private Boolean isTextureReady = false;
     private Object thisLock = new Object();
 
     volatile bool keepThreadAlive = true;
     private bool isSVRReady = false;
+
+    private int count = 0;
+
+    private static bool open = true;
 
     void Start()
     {
@@ -31,7 +35,6 @@ public class LoadRGB : MonoBehaviour, SvrManager.SvrEventListener
         SvrManager.Instance.AddEventListener (this);
         // set to 720p
         newThread = new Thread(GetRGBFrame);
-        newThread2 = new Thread(GetRGBFrame);
     }
 
     /// <summary>
@@ -47,6 +50,7 @@ public class LoadRGB : MonoBehaviour, SvrManager.SvrEventListener
                 Debug.Log("xslam_set_rgb_resolution");
                 API.xslam_set_rgb_resolution(1);
                 isSVRReady = true;
+                count = 0;
                 break;
         }
     }
@@ -67,26 +71,19 @@ public class LoadRGB : MonoBehaviour, SvrManager.SvrEventListener
                         {
                             if (API.xslam_get_rgb_image_RGBA(pixelPtr, tex.width, tex.height, ref rgbTimestamp))
                             {
-                                //Update the Texture2D with array updated in C++
-                                //tex.SetPixels32(pixel32);
-                                //tex.Apply();
+
                             }
-                            // else
-                            // {
-                            //     Debug.Log("Invalid texture");
-                            // }
                         }
                         catch (Exception e)
                         {
                             Debug.LogException(e, this);
-                            //yield return new WaitForSeconds(0.3f);
                             return;
                         }
-                        //Debug.Log("thread E id " + Thread.CurrentThread.ManagedThreadId);
                     }
                 }
             }
-            Thread.Sleep(65);
+            Thread.Sleep(30);
+            count++;
         }
     }
     
@@ -132,9 +129,9 @@ public class LoadRGB : MonoBehaviour, SvrManager.SvrEventListener
                     if(!keepThreadAlive) {
                         keepThreadAlive = true;
                         newThread.Start();
-                        newThread2.Start();
                     }
                 }
+
 
                 // if (GameObject.Find("TogglePanel").GetComponent<StreamToggle>().RgbOn())
                 // //if (true)
@@ -165,18 +162,62 @@ public class LoadRGB : MonoBehaviour, SvrManager.SvrEventListener
                     tex.SetPixels32(pixel32);
                     tex.Apply();
                     //Debug.Log("apply E");
+                    if(count == 50) {
+                        SavePNG();
+                        //count = 0;
+                        //API.xslam_start_rgb_stream();
+                    }
                 }
+
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            if(!open) {
+                open = true;
+                Debug.Log("xslam_start_rgb_stream");
+                API.xslam_start_rgb_stream();
+            } else {
+                open = false;
+                Debug.Log("xslam_stop_rgb_stream");
+                API.xslam_stop_rgb_stream();              
             }
         }
     }
 
+    void SavePNG()
+    {
+        string time = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+        string path = GetAndroidExternalStoragePath();
+        
+        byte[] bytes = tex.EncodeToPNG();
+        string tempPath1 = Path.Combine(path, time + "_texture.png");
+        File.WriteAllBytes(tempPath1, bytes);
+
+        byte[] destination = new byte[pixel32.Length * Marshal.SizeOf(typeof(Color32))];
+        Marshal.Copy(pixelPtr, destination, 0, destination.Length);
+        string tempPath2 = Path.Combine(path, time + "_.png");
+        File.WriteAllBytes(tempPath2, bytes);
+    }
+
+    private string GetAndroidExternalStoragePath()
+    {
+        if (Application.platform != RuntimePlatform.Android)
+            return Application.persistentDataPath;
+
+        var jc = new AndroidJavaClass("android.os.Environment");
+        var path = jc.CallStatic<AndroidJavaObject>("getExternalStoragePublicDirectory",
+            jc.GetStatic<string>("DIRECTORY_DCIM"))
+            .Call<string>("getAbsolutePath");
+        return path;
+    }
+
     void OnApplicationQuit()
     {
-        //Free handle
+        Debug.Log("OnApplicationQuit");
         newThread.Abort();
-        newThread2.Abort();
-        //newThread3.Abort();
-        //StopAllCoroutines();
+        //Free handle
         pixelHandle.Free();
     }
 }
